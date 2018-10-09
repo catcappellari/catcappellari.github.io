2978cc58bb479e9b104d11f1d49b1af145571b15
catcappellari <cat.cappellari@gmail.com>
2018-10-09 10:31:23 -0400

    header update

---
+++ header.php
@@ -0,0 +1,25 @@
+<a href="#main-content" class="focus-only js-focus-only js__jump">Skip to main content</a>
+<header class="header-main header-index">
+	<div class="c-contain c-contain_1400">
+		<div class="h-block h-block1">
+            <a href="index.html" class="logo"><img src="images/logo2.svg" alt="Logo - Cat Cappellari"></a>
+        </div>
+        <div class="h-block h-block2">
+        	<a href="mailto:cat.cappellari@gmail.com" class="email material-icons mi-white">email</a>
+            <nav class="mainmenu" role="navigation">
+                <button id="mainmenu_button" class="menu-button js-menu-button" aria-expanded="false" aria-controls="mainmenu">
+                	<span></span>
+                	<span></span>
+            		<span></span>
+            	</button>
+                <ul id="mainmenu" class="mobile-menu js-mobile-menu">
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="/sandbox">Sandbox</a></li>
+                    <li><a href="/resume">Resume</a></li>
+                </ul>
+            </nav>
+        </div>
+	</div>
+</header>
+<div id="main-content"></div>
